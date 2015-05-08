92a7db7d075dd82a76ea7fa770c3b9c2ccff6ce2
Kevin Campos <kevinmcampos@gmail.com>
2015-05-08 00:30:59 -0300

    Adicionado activity de configurações

--- app/src/main/java/br/com/cafecomcodigo/myweatherapp/MainActivity.java
+++ app/src/main/java/br/com/cafecomcodigo/myweatherapp/MainActivity.java
@@ -1,26 +1,10 @@
 package br.com.cafecomcodigo.myweatherapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -53,6 +37,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
