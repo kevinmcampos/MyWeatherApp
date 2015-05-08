1918cb83c75f54560596dfa815456be3d149f333
Kevin Campos <kevinmcampos@gmail.com>
2015-05-08 17:27:54 -0300

    Adicionado arquivos de banco

--- app/src/main/java/br/com/cafecomcodigo/myweatherapp/MainActivity.java
+++ app/src/main/java/br/com/cafecomcodigo/myweatherapp/MainActivity.java
@@ -24,8 +24,38 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new ForecastFragment())
                     .commit();
         }
+        Log.v(LOG_TAG, "onCreate");
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(LOG_TAG, "onDestroy");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
